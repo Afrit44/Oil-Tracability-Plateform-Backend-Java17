dd9494f8f5d71c9c28dbfcbc157a1dff0e198d78
Arij Hm <h.arij@live.fr>
2022-12-07 23:40:09 +0100

    Adding Services and implementations.

--- src/main/java/com/Oil4Med/Oil4Med/Service/Impl/StorageAreaImpl.java
+++ src/main/java/com/Oil4Med/Oil4Med/Service/Impl/StorageAreaImpl.java
@@ -1,4 +1,35 @@
 package com.Oil4Med.Oil4Med.Service.Impl;
 
-public class StorageAreaImpl {
+import com.Oil4Med.Oil4Med.Model.StorageArea;
+import com.Oil4Med.Oil4Med.Service.StorageAreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class StorageAreaImpl implements StorageAreaService {
+
+    @Override
+    public List<StorageArea> getStorageArea() {
+        return null;
+    }
+
+    @Override
+    public StorageArea getStorageAreaById(Long id) {
+        return null;
+    }
+
+    @Override
+    public StorageArea addStorageArea(StorageArea storageArea) {
+        return null;
+    }
+
+    @Override
+    public void deleteStorageArea(StorageArea storageArea) {
+
+    }
+
+    @Override
+    public void updateStorageArea(Long id, StorageArea storageArea) {
+
+    }
 }

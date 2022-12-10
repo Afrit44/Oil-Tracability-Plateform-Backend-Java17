72a8bb715bf293c5b864d5ef200034886e4fd26f
Arij Hm <h.arij@live.fr>
2022-12-10 12:16:46 +0100

    .

--- src/main/java/com/Oil4Med/Oil4Med/Controller/ConsumerController.java
+++ src/main/java/com/Oil4Med/Oil4Med/Controller/ConsumerController.java
@@ -1,14 +1,21 @@
 package com.Oil4Med.Oil4Med.Controller;
 
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.Oil4Med.Oil4Med.Service.ConsumerService;
+import com.Oil4Med.Oil4Med.Service.PurchaseOilService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
-@RequestMapping("/api/oil4med/consumer/")
+//@CrossOrigin
+@RequestMapping("/api/consumer/")
 public class ConsumerController {
 
+    @Autowired
+    private ConsumerService consumerService;
+
+    @Autowired
+    private PurchaseOilService purchaseOilService;
 
 
 }
---
+++ src/main/java/com/Oil4Med/Oil4Med/Controller/MillManagerController.java
@@ -0,0 +1,4 @@
+package com.Oil4Med.Oil4Med.Controller;
+
+public class MillManagerController {
+}

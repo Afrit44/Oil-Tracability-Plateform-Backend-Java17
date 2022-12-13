d354e53ea4f477c7b8a09f21281c46276cdbc1f6
Arij Hm <h.arij@live.fr>
2022-12-13 14:31:27 +0100

    Solving Swagger-related errors.

--- src/main/java/com/Oil4Med/Oil4Med/Configuration/SecurityConfig.java
+++ src/main/java/com/Oil4Med/Oil4Med/Configuration/SecurityConfig.java
@@ -1,54 +1,55 @@
-//package com.Oil4Med.Oil4Med.Configuration;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@EnableWebSecurity
-//
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//
-//    //this part makes sure that every user should pass through the authentication as a first step
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//                .authorizeHttpRequests().antMatchers("/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and().sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        http.authorizeRequests()
-//                .antMatchers(
-//                        "/v2/api-docs",
-//                        "/swagger-resources/**",
-//                        "/swagger-ui.html",
-//                        "/webjars/**" ,
-//                        /*Probably not needed*/ "/swagger.json")
-//                .permitAll();
+package com.Oil4Med.Oil4Med.Configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+public class SecurityConfig{
+
+
+    //this part makes sure that every user should pass through the authentication as a first step
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests().antMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        /*http.authorizeRequests()
+                .antMatchers(
+                        "/v2/api-docs",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/webjars/**" ,
+                        *//*Probably not needed*//* "/swagger.json")
+                .permitAll();*/
+        http.authorizeRequests().antMatchers("/swagger/**","/v3/**").permitAll();
+    }
+
+
+//    @Bean
+//    public AuthenticationManager authenticationManager() throws Exception {
+//        return super.authenticationManager();
 //    }
 //
-//
-////    @Bean
-////    public AuthenticationManager authenticationManager() throws Exception {
-////        return super.authenticationManager();
-////    }
-////
-////    @Bean
-////    public PasswordEncoder passwordEncoder() {
-////        return new BCryptPasswordEncoder();
-////    }
-//
-////    @Bean
-////    public ActiveUserStore activeUserStore(){
-////        return new ActiveUserStore();
-////    }
-//
-//}
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+//    @Bean
+//    public ActiveUserStore activeUserStore(){
+//        return new ActiveUserStore();
+//    }
+
+}
